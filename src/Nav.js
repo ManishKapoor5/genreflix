62c665b454d78b4c9e547fa4d90ae9e918eea85b
Manish Kapoor <115330437+ManishKapoor5@users.noreply.github.com>
2022-11-24 05:27:33 +0530

    Commit deleted

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function nav() {
+  return (
+    <div className="nav">
+      <img
+      className="nav__logo"
+      src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
+      alt="Netflix logo"
+      />
+</div>
+  )
+}
+
+export default nav
